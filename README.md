17794c4c9353112bdddae5af26a683bccd03017f
Andy Pymont <andypymont@gmail.com>
2014-08-12 00:31:40 +0100

    Inception database set up and players now loaded from it

---
+++ README.md
@@ -0,0 +1,7 @@
+## Synopsis
+
+Web application for my Premier League fantasy football game. Written in the Flask web framework for Python.
+
+## License
+
+http://www.gnu.org/licenses/gpl.txt
--- fantasypl.py
+++ fantasypl.py
@@ -10,7 +10,7 @@ app.config.update(dict(DEBUG=True))
 def index():
 	with open(os.path.join('db', 'teams.json')) as f:
 		players = json.loads(f.read())
-	players = sorted([player for player in players if player['team'] == 'Laura'],
+	players = sorted([player for player in players if player['team'] == 'Dave'],
 				     key=lambda player: ({'G': 1, 'D': 2, 'M': 3, 'F': 4}[player['position']], player['name']))
 	return render_template('index.html', players=players)
 
