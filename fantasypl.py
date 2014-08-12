26b299d2d645c1fe6b489d63688ba1ec40656c48
Andy Pymont <andypymont@gmail.com>
2014-08-12 06:15:25 +0100

    Moved lineup to page /lineup URL and added (currently non-state-changing) form submit

--- fantasypl.py
+++ fantasypl.py
@@ -2,17 +2,21 @@ import json
 import os
 
 from app import app, db
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 app.config.update(dict(DEBUG=True))
 
-@app.route('/')
-def index():
+@app.route('/lineup/')
+def lineup():
 	with open(os.path.join('db', 'teams.json')) as f:
 		players = json.loads(f.read())
-	players = sorted([player for player in players if player['team'] == 'Dave'],
+	players = sorted([player for player in players if player['team'] == 'Andy'],
 				     key=lambda player: ({'G': 1, 'D': 2, 'M': 3, 'F': 4}[player['position']], player['name']))
-	return render_template('index.html', players=players)
+	return render_template('lineup.html', players=players)
+
+@app.route('/lineup/submit', methods=['POST'])
+def lineup_submit():
+	return redirect(url_for('lineup'))
 
 if __name__ == '__main__':
 	app.run()
