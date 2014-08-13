295058c97f8198759cf3c6566a8a6d37afd2b8be
Andy Pymont <andypymont@gmail.com>
2014-08-13 21:37:56 +0100

    Updated newuser helper function in manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,23 @@
+from app import app, db
+from auth import generate_password_hash
 from flask.ext.script import Manager
-from app import app
+import random
+import string
 
 manager = Manager(app)
+
+@manager.command
+def newuser(name, username, password, draftorder=0, token=None):
+	"Create a new user"
+
+	tokens = [user['token'] for user in db.get('users')]
+	while (token is None) or (token in tokens):
+		token = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in xrange(20))
+
+	user = dict(userid=username, password=generate_password_hash(password), name=name, token=token,
+				wins=0, draws=0, losses=0, points=0, score=0, draftorder=draftorder)
+	db.save(user, 'users')
+	return token
 
 if __name__ == '__main__':
 	manager.run()
