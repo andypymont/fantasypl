6cc8d2ada319aac393fca346d4b862a0dd15a469
Andy Pymont <andypymont@gmail.com>
2014-08-19 20:33:21 +0100

    Added gameweeks with their respective deadlines; reject team submission if after deadline

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ import os
 
 from app import app, db
 from auth import current_user, login_manager, login_required
+from datetime import datetime
 from flask import abort, flash, redirect, render_template, request, url_for
 from math import ceil
 from unidecode import unidecode
@@ -17,6 +18,12 @@ def get_lineup(team):
 
 def next_opponents():
 	return dict([(club['name'], club['nextopponent']) for club in db.get('clubs')])
+
+def current_gameweek():
+	rv = sorted(db.get('gameweeks', {'completed': False}), key=lambda gw: gw['week'])[0]
+	rv['deadline'] = datetime.strptime(rv['deadline'], '%Y-%m-%dT%H:%M:%S')
+	rv['waiver'] = datetime.strptime(rv['waiver'], '%Y-%m-%dT%H:%M:%S')
+	return rv
 
 def formation(players):
 	rv = dict()
@@ -50,6 +57,14 @@ def pagination(current_page, pages):
 				prev=(current_page > 1),
 				next=(current_page < pages))
 
+def server_time():
+	now = datetime.now()
+	return datetime(now.year, now.month, now.day, now.hour, now.minute)
+
+@app.template_filter('datetime_deadline')
+def filter_datetime_deadline(dt):
+	return dt.strftime('%a %d/%m/%Y %H:%M')
+
 @app.route('/')
 @app.route('/standings/')
 def standings():
@@ -63,26 +78,32 @@ def standings():
 def lineup():
 	if current_user:
 		players = get_lineup(current_user.get_name())
+	deadline = current_gameweek()['deadline']
 	return render_template('lineup.html', players=players, activepage="lineup", current_user=current_user,
-										  next_opponents=next_opponents())
+										  next_opponents=next_opponents(), deadline=deadline, servertime=server_time())
 
 @app.route('/lineup/submit', methods=['POST'])
 def lineup_submit():
 	if not current_user:
 		abort(401)
 	else:
-		players = get_lineup(current_user.get_name())
-		for player in players:
-			if request.form.get('startercheck%s' % player['_id'], '') == 'on':
-				player['startingxi'] = '1'
-			else:
-				player['startingxi'] = '0'
+		deadline = current_gameweek()['deadline']
 
-		if valid_formation(player for player in players if player['startingxi'] == '1'):
-			db.save_all(players, 'players')
-			flash('Lineup saved')
+		if deadline < datetime.now():
+			flash("This week's lineup deadline has passed. Your lineup changes have not been saved.")
 		else:
-			flash('Lineup reverted as was not a valid formation (please use 1 G, 3-5 D, 3-5 M, 1-3 F)')
+			players = get_lineup(current_user.get_name())
+			for player in players:
+				if request.form.get('startercheck%s' % player['_id'], '') == 'on':
+					player['startingxi'] = '1'
+				else:
+					player['startingxi'] = '0'
+
+			if valid_formation(player for player in players if player['startingxi'] == '1'):
+				db.save_all(players, 'players')
+				flash('Lineup saved')
+			else:
+				flash('Lineup reverted as was not a valid formation (please use 1 G, 3-5 D, 3-5 M, 1-3 F)')
 
 		return redirect(url_for('lineup'))
 
